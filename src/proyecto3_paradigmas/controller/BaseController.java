ea0f32526ea67330eb82dbf6c31ab7268a3cb918
Gerardo <gerardo.araya.ceciliano@est.una.ac.cr>
2021-11-05 23:02:55 -0600

    :V

--- src/proyecto3_paradigmas/controller/BaseController.java
+++ src/proyecto3_paradigmas/controller/BaseController.java
@@ -26,6 +26,7 @@ import proyecto3_paradigmas.util.PadronUtils;
 
 /**
  * Esta clase se encarga de controlar los eventos dentro de la vista Base.fxml.
+ *
  * @author Roberth
  * @author Gerardo
  * @author Andres
@@ -63,7 +64,6 @@ public class BaseController implements Initializable {
     @FXML
     private TextField txt_apellido2;
 
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
@@ -77,6 +77,9 @@ public class BaseController implements Initializable {
         // TODO
     }
 
+    /**
+     * Prepara la tabla para poder insertar datos tipo persona
+     */
     private void prepararTabla() {
         activarResponsiveTable();
         tbl_personas.setItems(listaTabla);  //Lista de la tabla ahora es lista externa
@@ -89,8 +92,8 @@ public class BaseController implements Initializable {
         cl_cedula.setCellValueFactory(x
                 -> new SimpleStringProperty(x.getValue().getCedula()));
         cl_apellidos.setCellValueFactory(x
-                -> new SimpleStringProperty(x.getValue().getApellido1() + " " +
-                        x.getValue().getApelido2()));
+                -> new SimpleStringProperty(x.getValue().getApellido1() + " "
+                        + x.getValue().getApelido2()));
         cl_fecha_vencimiento.setCellValueFactory(x
                 -> new SimpleStringProperty(x.getValue().getFechaVencimiento()));
         cl_estado_cedula.setCellValueFactory(x
@@ -98,25 +101,32 @@ public class BaseController implements Initializable {
 
     }
 
-
+    /**
+     * Permite realizar la busqueda de busqueda mediante diferentes filtros
+     *
+     * @param event
+     */
     @FXML
     private void buscar(ActionEvent event) {
 
-       listaTabla.clear();
-       listaTabla.addAll(listaBase.stream().filter(itm -> 
-            itm.getNombre().
-               contains(txt_nombre.getText().trim().toUpperCase()) &&
-            itm.getApellido1().
-                    contains(txt_apellidos.getText().trim().toUpperCase()) &&
-            itm.getApelido2().
-                    contains(txt_apellido2.getText().trim().toUpperCase()) &&
-            itm.getCedula().toUpperCase().
-                    contains(txt_cedula.getText().trim().toUpperCase()) &&
-            itm.getFechaVencimiento().
-                    contains((dp_fechaVecimiento.getValue() != null) ?
-                            dp_fechaVecimiento.getValue().toString() : "")
+        listaTabla.clear();
+        listaTabla.addAll(listaBase.stream().filter(itm
+                -> itm.getNombre().
+                        contains(txt_nombre.getText().trim().toUpperCase())
+                && itm.getApellido1().
+                        contains(txt_apellidos.getText().trim().toUpperCase())
+                && itm.getApelido2().
+                        contains(txt_apellido2.getText().trim().toUpperCase())
+                && itm.getCedula().toUpperCase().
+                        contains(txt_cedula.getText().trim().toUpperCase())
+                && itm.getFechaVencimiento().
+                        contains((dp_fechaVecimiento.getValue() != null)
+                                ? dp_fechaVecimiento.getValue().toString() : "")
         ).collect(Collectors.toList()));
     }
+    /**
+     * Permite hacer la tabla responsi
+     */
 
     private void activarResponsiveTable() {
         cl_apellidos.prefWidthProperty().
@@ -131,41 +141,98 @@ public class BaseController implements Initializable {
                 bind(tbl_personas.widthProperty().divide(6));
         cl_estado_cedula.prefWidthProperty().
                 bind(tbl_personas.widthProperty().divide(6));
-        
+
     }
+    /**
+     * Permite llamar al meto que realiza la busqueda de conincidencias 
+     * @param event 
+     */
 
     @FXML
     private void buscarConcidencias(ActionEvent event) {
-        List<Persona> aux = filtroList(listaBase);
+        List<Persona> coincidencias = filtroListCoincidencias(listaBase);
         listaTabla.clear();
         Mensaje ms = new Mensaje();
-        ms.showInformation("Cantidad de personas con el nombre " + txt_nombre.getText()
-                + ": " + aux.size());
-        listaTabla.addAll(aux);
+        ms.showInformation("Cantidad de personas por concidencia"
+                + ": " + coincidencias.size());
+        listaTabla.addAll(coincidencias);
     }
 
-    //devuelve una lista filtrada por nombre
-    private List<Persona> filtroList(List<Persona> list) {
+    /**
+     * retorna las persona que cumple con la condecia
+     *
+     * @param list resive la lista de personas
+     * @return retorna la lista filtrada
+     */
+    private List<Persona> filtroListCoincidencias(List<Persona> list) {
 
+        if (txt_nombre.getText().length() != 0 && txt_apellidos
+                .getText().length() != 0
+                && txt_apellido2.getText().length() != 0) {
+            return list.stream().filter(x -> {
+                return (txt_nombre.getText().toUpperCase().
+                        equals(x.getNombre())
+                        && (txt_apellidos.getText().toUpperCase()
+                                .equals(x.getApellido1()))
+                        && txt_apellido2.getText().toUpperCase().
+                                equals(x.getApelido2()));
+            }).collect(Collectors.toList());
+        }
         if (txt_nombre.getText().length() != 0
-                && txt_apellidos.getText().length() == 0) {
+                && txt_apellidos.getText().length() == 0
+                && txt_apellido2.getText().length() == 0) {
             return list.stream().filter(x -> {
-                return (txt_nombre.getText().toUpperCase().equals(x.getNombre()));
-            }).collect(Collectors.toList());
-        } else if (txt_nombre.getText().length() == 0
-                && txt_apellidos.getText().length() != 0) {
-            return list.stream().filter(x -> {
-                return (txt_apellidos.getText().toUpperCase().equals(
-                                x.getApellido1() + " " + x.getApelido2()));
-            }).collect(Collectors.toList());
-        } else {
-            return list.stream().filter(x -> {
-                return (txt_nombre.getText().toUpperCase().equals(x.getNombre())
-                        && txt_apellidos.getText().toUpperCase().equals(
-                                x.getApellido1() + " " + x.getApelido2()));
+                return (txt_nombre.getText().toUpperCase().
+                        equals(x.getNombre()));
             }).collect(Collectors.toList());
         }
 
+        if (txt_nombre.getText().length() != 0
+                && txt_apellidos.getText().length() != 0
+                && txt_apellido2.getText().length() == 0) {
+            return list.stream().filter(x -> {
+                return (txt_nombre.getText().toUpperCase().
+                        equals(x.getNombre())
+                        && (txt_apellidos.getText().toUpperCase()
+                                .equals(x.getApellido1())));
+            }).collect(Collectors.toList());
+        }
+        if (txt_nombre.getText().length() != 0
+                && txt_apellidos.getText().length() == 0
+                && txt_apellido2.getText().length() != 0) {
+            return list.stream().filter(x -> {
+                return (txt_nombre.getText().toUpperCase().
+                        equals(x.getNombre())
+                        && txt_apellido2.getText()
+                                .toUpperCase().equals(x.getApelido2()));
+            }).collect(Collectors.toList());
+        }
+        if (txt_nombre.getText().length() == 0
+                && txt_apellidos.getText().length() != 0
+                && txt_apellido2.getText().length() != 0) {
+            return list.stream().filter(x -> {
+                return ((txt_apellidos.getText().toUpperCase()
+                        .equals(x.getApellido1()))
+                        && txt_apellido2.getText()
+                                .toUpperCase().equals(x.getApelido2()));
+            }).collect(Collectors.toList());
+        }
+         if (txt_nombre.getText().length() == 0
+                && txt_apellidos.getText().length() != 0
+                && txt_apellido2.getText().length() == 0) {
+            return list.stream().filter(x -> {
+                return ((txt_apellidos.getText().toUpperCase()
+                        .equals(x.getApellido1())));
+            }).collect(Collectors.toList());
+        }
+          if (txt_nombre.getText().length() == 0
+                && txt_apellidos.getText().length()==0
+                && txt_apellido2.getText().length() != 0) {
+            return list.stream().filter(x -> {
+                return ((txt_apellido2.getText().toUpperCase().equals(x.getApelido2())));
+            }).collect(Collectors.toList());
+        }
+        return null;
     }
 
 }
